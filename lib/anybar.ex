c92dbef35f4387726e77319767b47516052d2379
Bruno Buccolo <bruno.buccolo@gmail.com>
2017-02-12 20:27:05 -0200

    Adds docs to anybar.ex

--- lib/anybar.ex
+++ lib/anybar.ex
@@ -1,10 +1,18 @@
 defmodule AnyBar do
   @default_port 1738
+
+  @doc """
+  Changes `color` of AnyBar running on given `port`
+  """
+  @spec change(String.t, Integer.t) :: :ok | { :error, String.t }
   def change(color, port \\ @default_port, udp_client \\ :gen_udp) do
     {:ok, socket } = udp_client.open(0, [:binary])
     udp_client.send(socket, 'localhost', port, color)
   end
 
+  @doc """
+  Returns the default port of AnyBar
+  """
   def default_port do
     @default_port
   end
